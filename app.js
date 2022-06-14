363f5c459f2a98a171ca6f455c143a8e36ee1f5d
21cloudocean <437508630@qq.com>
2022-06-14 12:18:26 +0800

    alert+addItem+default+clear items+access to edit and delete btn

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const alert = document.querySelector(".alert");
 const form = document.querySelector(".grocery-form");
 const grocery = document.querySelector("#grocery");
-const submitBtn = document.querySelector("submit-btn");
+const submitBtn = document.querySelector(".submit-btn");
 const container = document.querySelector(".grocery-container");
 const list = document.querySelector(".grocery-list");
 const clearBtn = document.querySelector(".clear-btn");
@@ -13,6 +13,8 @@ let editID = "";
 // ****** EVENT LISTENERS **********
 //submit form
 form.addEventListener("submit", addItem);
+//clear items
+clearBtn.addEventListener("click", clearItems);
 // ****** FUNCTIONS **********
 function addItem(e) {
   e.preventDefault();
@@ -21,7 +23,38 @@ function addItem(e) {
   const id = new Date().getTime().toString();
   //   if(value !== '' && editFlag === false){ }
   if (value && !editFlag) {
-    console.log("add to the list");
+    //在list中创建item
+    const element = document.createElement("article");
+    //添加class
+    element.classList.add("grocery-item");
+    //添加id
+    const attribute = document.createAttribute("data-id");
+    attribute.value = id;
+    element.setAttributeNode(attribute);
+    element.innerHTML = `<p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    //在这里access btns才有效。
+    const deleteBtn = element.querySelector(".delete-btn");
+    const editBtn = element.querySelector(".edit-btn");
+    deleteBtn.addEventListener("click", deleteItem);
+    editBtn.addEventListener("click", editItem);
+    // append child（这一步用来display，只有放在HTML中才能看见）
+    list.appendChild(element);
+    //display alert
+    displayAlert("Item added to the list", "success");
+    //show container（添加解除hidden的class）
+    container.classList.add("show-container");
+    //add to local sotrge
+    addToLocalStorage(id, value);
+    //set back to default
+    setBackToDefault();
   }
   //   else if(value !=='' && editFlag === true){}
   else if (value && editFlag) {
@@ -30,7 +63,7 @@ function addItem(e) {
     displayAlert("please enter value", "danger");
   }
 }
-//display alert
+//display alert：text用来写显示内容，action用来控制class名，从而显示不同颜色
 function displayAlert(text, action) {
   alert.textContent = text;
   alert.classList.add(`alert-${action}`);
@@ -40,6 +73,41 @@ function displayAlert(text, action) {
     alert.classList.remove(`alert-${action}`);
   }, 1000);
 }
+// clear items
+function clearItems() {
+  const items = document.querySelectorAll(".grocery-item");
+  //如果有item，则清空：清空办法，用forEach一个一个从parent中删除
+  if (items.length > 0) {
+    items.forEach(function (item) {
+      list.removeChild(item);
+    });
+  }
+  //清空后没有了item，让container恢复没有提交任何item的状态，即hidden
+  container.classList.remove("show-container");
+  //alert提示删除成功
+  displayAlert("empty list", "success");
+  //操作页面初始化
+  setBackToDefault();
+  //将list从localStorage中删除
+  // localStorage.removeItem('list');
+}
+// delete function
+function deleteItem() {
+  console.log("item deleted");
+}
+//edit function
+function editItem() {
+  console.log("edit item");
+}
+//set back to default
+function setBackToDefault() {
+  grocery.value = "";
+  editFlag = false;
+  editID = "";
+  submitBtn.textContent = "submit";
+}
 // ****** LOCAL STORAGE **********
-
+function addToLocalStorage(id, value) {
+  console.log("added to local storage");
+}
 // ****** SETUP ITEMS **********
